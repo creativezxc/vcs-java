9d7dd05c72091c8b96f7c68d9fae8dbd6c5e3661
ERNEST\Ernest <creativezxc@gmail.com>
2016-11-15 22:22:07 +0200

    Padariau namu darbus

---
+++ Paskaita2nd/src/lt/vcs/Paskaita2nd.java
@@ -0,0 +1,16 @@
+package lt.vcs;
+
+/**
+ *
+ * @author Ernest
+ */
+public class Paskaita2nd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Padariau namu darbus");
+    }
+    
+}
