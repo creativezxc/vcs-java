9d6a19e778f8231577bc49e18c2917a6a21b3e51
ERNEST\Ernest <creativezxc@gmail.com>
2016-11-16 18:37:46 +0200

    Paskaita3

---
+++ Paskaita3/src/lt/vcs/Main.java
@@ -0,0 +1,16 @@
+package lt.vcs;
+
+/**
+ *
+ * @author Ernest
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
